89fae26182a19292155e68a1a5380cb1ca23ba00
Gevorg Arzakanyan <zokavski@gmail.com>
2025-07-30 19:01:17 +0400

    task 6 is done

---
+++ script.js
@@ -0,0 +1,102 @@
+// --------------- TASK 1
+
+const input = document.getElementById("input");
+
+const printWithDelay = debounce((e) => {
+  console.log(e.target.value);
+}, 500);
+
+input.addEventListener("input", printWithDelay);
+
+function debounce(fn, delay = 400) {
+  let timerId;
+  return function (...args) {
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+// ----------------------TASTK 2
+
+const tripleButton = document.getElementById("curied-button");
+
+tripleButton.addEventListener("click", () => console.log(sum(5)(3)(2)));
+
+function sum(a) {
+  return (b) => {
+    return (c) => a + b + c;
+  };
+}
+
+// ----------------------------TASK 3
+
+const hibutton = document.getElementById("hi-button");
+
+hibutton.addEventListener("click", () => {
+  sayHi("John");
+});
+
+function greet(greeting) {
+  return (name) => console.log(`${greeting}, ${name}`);
+}
+
+const sayHi = greet("Hi");
+
+// -------------------------------TASK 4
+
+const user1 = {
+  name: "Alice",
+  greet() {
+    return `Hello, I'm ${this.name}`;
+  },
+};
+
+const user2 = {
+  name: "Bob",
+};
+
+console.log(user1.greet.call(user2));
+
+// --------------------------------TASK 5
+
+const counter = {
+  count: 0,
+  increment() {
+    this.count++;
+    console.log(this.count);
+  },
+};
+
+const increment = counter.increment.bind(counter);
+
+const clickMe = document.getElementById("clickBtn");
+
+clickMe.addEventListener("click", increment);
+
+// --------------------------------------TASK 6
+
+let arr = [1, 2, 3, 6, 6, 8, 3, 3, 9, 10, 12, 12];
+
+let set = new Set(arr);
+
+console.log([...set]);
+
+let arr2 = [1, 1, 1, 1, 1, 2, 3, 4, 3, 4, 4];
+
+function deleteDuplicate(arr) {
+  let arrN = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arrN.includes(arr[i])) {
+      continue;
+    } else {
+      arrN.push(arr[i]);
+    }
+  }
+  return arrN;
+}
+
+console.log(deleteDuplicate(arr2));
+
+// ---------------------------------------- TASK 7
